c8d65ba2b744221c4c62d4ced73b2f90ff5193e1
Scott Lee <yle4@cdc.gov>
2022-03-16 13:13:24 -0400

    adding some docstrings

--- tools/optimization.py
+++ tools/optimization.py
@@ -1,3 +1,5 @@
+"""Functions used for the various kinds of optimizations"""
+
 import numpy as np
 import scipy as sp
 import pandas as pd
@@ -6,6 +8,7 @@ from scipy.special import expit, erf
 
 
 def unique_combo(c):
+    """Determines if a combination of symptoms is unique."""
     if len(np.intersect1d(c[0], c[1])) == 0:
         return c
     else:
@@ -13,30 +16,36 @@ def unique_combo(c):
 
 
 def smash_log(x, B=10, d=0):
+    """Logistic function with a little extra kick."""
     return 1 / (1 + np.exp(-x * B)) - d
 
 
-def sens(z, xp=xp, B=100):
+def sens(z, xp, B=100):
+    """Calculates sensitivity, or true positive rate."""
     m = z[-1]
     z = z[:-1]
     return smash_log(np.dot(xp, z) - m, B=B).sum() / xp.shape[0]
 
 
-def spec(z, xn=xn, B=100):
+def spec(z, xn, B=100):
+    """Calculates specificity, or 1 minus the false positive rate."""
     m = z[-1]
     z = z[:-1]
     return 1 - smash_log(np.dot(xn, z) - m, B=B).sum() / xn.shape[0]
 
 
 def j_lin(z, xp, xn, m):
+    """Calculates Youden's J index from binary guesses."""
     z = np.round(z)
     tpr = np.sum(np.dot(xp, z) >= m) / xp.shape[0]
     fpr = np.sum(np.dot(xn, z) >= m) / xn.shape[0]
-    print(tpr, fpr)
     return tpr - fpr
 
 
 def j_exp(z, xp, xn, a=1, b=1):
+    """Calculates Youden's J index for a single m-of-n rule using the 
+    parameters of a solved LP (z) and the smash_log() function.
+    """
     m = z[-1]
     z = smash_log(z[:-1] - .5)
     tpr = smash_log(np.dot(xp, z) - m + .5).sum() / xp.shape[0]
@@ -45,6 +54,9 @@ def j_exp(z, xp, xn, a=1, b=1):
 
 
 def j_exp_comp(z, xp, xn, c=2, a=1, b=1, th=0):
+    """Calculates Youden's J index for a compound m-of-n rule using the
+    parameters of a solved LP (z) and the smash_log() function.
+    """
     # Setting things up
     s = xp.shape[1]
     m = z[-c:]
@@ -69,6 +81,9 @@ def j_exp_comp(z, xp, xn, c=2, a=1, b=1, th=0):
 
 
 def j_lin_comp(n_mat, m_vec, X, y):
+    """Calculates Youden's J index from the N matrix and M vector specifying
+    a given compound rule. Both must be binary.
+    """
     counts = np.array([np.dot(X, v) for v in n_mat.T]).T
     diffs = np.array([counts[:, i] - m_vec[i] >= 0 
                       for i in range(len(m_vec))])
@@ -77,11 +92,12 @@ def j_lin_comp(n_mat, m_vec, X, y):
     return j
 
 
-def m_morethan_n(z, c=Nc, s=Ns):
-    m = z[-c:]
-    z = z[:-c]
-    z = z.reshape((s, c), order='F')
+def m_morethan_n(z, Nc, Ns):
+    """Determines whether m is more than n for a given m-of-n rule."""
+    m = z[-Nc:]
+    z = z[:-Nc]
+    z = z.reshape((Ns, Nc), order='F')
     z = smash_log(z - .5, B=15)
     nvals = z.sum(0)
     diffs = smash_log(nvals - m + .5)
-    return (c - diffs.sum())
+    return (Nc - diffs.sum())
