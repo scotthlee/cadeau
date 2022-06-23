613118be3c7ce490c0479bffbc3fcfa27b7d154d
Scott Lee <yle4@cdc.gov>
2022-06-23 10:49:02 -0400

    stuff

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import tracemalloc
 import time
 import seaborn as sns
 
@@ -16,8 +17,7 @@ records = pd.read_csv('data/tb_data.csv')
 # Making them combined
 y = records.mtb.values.astype(np.uint8)
 X = records.iloc[:, records.columns != 'mtb'].fillna(0)
-for c in X.columns.values:
-    X[c] = X[c].astype(np.uint8)
+X = X.astype(np.uint8)
 
 # Setting up a pruner to whittle down the variables
 pruner = ops.FeaturePruner()
@@ -36,11 +36,22 @@ nola.fit(X, y)
 end = time.time()
 nola_time = end - start
 
+tracemalloc.start()
 start = time.time()
 fe = ops.FullEnumeration()
-fe.fit(X, y, max_n=None)
+fe.fit(pruner.X, y, chunksize=int(1e4))
 end = time.time()
+fe_size, fe_peak = tracemalloc.get_traced_memory()
 fe_time = end - start
+
+tracemalloc.clear_traces()
+tracemalloc.start()
+start = time.time()
+fe_share = ops.FullEnumeration(share_memory=True)
+fe_share.fit(pruner.X, y, chunksize=int(1e4))
+end = time.time()
+fe_share_size, fe_share_peak = tracemalloc.get_traced_memory()
+fe_share_time = end - start
 
 # Trying the full enumeration and approximation on the compound problem
 start = time.time()
