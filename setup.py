4ccc3748211bed074b29f1f4bfba89a8a4328ecf
Scott Lee <yle4@cdc.gov>
2022-08-03 13:23:22 -0400

    changing setup

--- setup.py
+++ setup.py
@@ -12,70 +12,32 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+#with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    #long_description = f.read()
 
 setup(
     name='kudos',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
-
-    description='Case definition optimization for public health',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-
-    # The project's main homepage.
+    version='0.0.1',
+    description='Tools for optimizing public health case definitions',
+    #long_description=long_description,
+    #long_description_content_type='text/markdown',
     url='https://github.com/scotthlee/kudos',
-
-    # Author details
     author='Scott Lee',
     author_email='yle4@cdc.gov',
-
-    # Choose your license
     license='Apache 2.0',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Epidemiologists',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
-
-    # What does your project relate to?
     keywords='combinatorial optimization',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(),
-    #package_dir={'': '.'},
-    #packages=[''],
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #py_modules=['saliency'],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
+    packages=['kudos'],
     install_requires=[
         'matplotlib', 'numpy', 'ortools',
         'pandas', 'scikit-learn', 'scipy',
         'seaborn', 'tqdm'
-    ]
+    ],
 )
